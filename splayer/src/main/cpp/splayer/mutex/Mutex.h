8b1612838fd6e90ad44853add15f284ea884f18d
biezhihua <biezhihua@gmail.com>
2019-06-12 20:39:10 +0800

    mutex

---
+++ splayer/src/main/cpp/splayer/mutex/Mutex.h
@@ -0,0 +1,62 @@
+#ifndef SPLAYER_MUTEX_H
+#define SPLAYER_MUTEX_H
+
+#include <pthread.h>
+#include <assert.h>
+#include <errno.h>
+#include <sys/time.h>
+
+#define MUTEX_TIMEDOUT  1
+#define MUTEX_MAXWAIT   (~(uint32_t)0)
+
+class Mutex {
+
+private:
+    pthread_mutex_t mutex;
+    pthread_cond_t cond;
+
+public:
+    Mutex() {
+        pthread_mutex_init(&mutex, NULL);
+        pthread_cond_init(&cond, NULL);
+    }
+
+    ~Mutex() {
+        pthread_cond_destroy(&cond);
+        pthread_mutex_destroy(&mutex);
+    }
+
+    /**
+     * lock a mutex
+     */
+    int mutexLock();
+
+
+    /**
+     * unlock a mutex
+     */
+    int mutexUnLock();
+
+    /**
+     * Signal a condition
+     */
+    int condSignal();
+
+    /**
+     * Broadcast a condition
+     */
+    int condBroadcast();
+
+    /**
+     * Wait on a condition with timeout
+     */
+    int condWaitTimeout(uint32_t ms);
+
+    /**
+     * Wait on a condition
+     */
+    int condWait();
+};
+
+
+#endif //SPLAYER_MUTEX_H
