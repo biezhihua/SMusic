9471f592b349f46cabf1ac8a06510b78ee4750cf
biezhihua <biezhihua@gmail.com>
2019-06-17 11:46:59 +0800

    fix: compile error

--- splayer/src/main/cpp/SMainJni.cpp
+++ splayer/src/main/cpp/SMainJni.cpp
@@ -14,15 +14,15 @@
 #pragma clang diagnostic ignored "-Wunused-parameter"
 
 JavaVM *sJavaVM = nullptr;
-SPlayer *sPlayer = nullptr;
-SStatus *sStatus = nullptr;
-SJavaMethods *sJavaMethods = nullptr;
+//SPlayer *sPlayer = nullptr;
+//SStatus *sStatus = nullptr;
+//SJavaMethods *sJavaMethods = nullptr;
 MediaPlayer *mediaPlayer = nullptr;
 #define JNI_TAG "Native_MainJni"
 
 // Destroy Instance
-pthread_t sDestroyThread;
-bool sIsExiting = false;
+//pthread_t sDestroyThread;
+//bool sIsExiting = false;
 
 JNIEXPORT jint JNI_OnLoad(JavaVM *vm, void *reserved) {
     LOGD(JNI_TAG, "JNI_OnLoad");
@@ -42,9 +42,9 @@ JNIEXPORT void JNI_OnUnload(JavaVM *vm, void *reserved) {
 extern "C" JNIEXPORT void JNICALL
 Java_com_bzh_splayer_lib_SPlayer_nativeSetSource(JNIEnv *env, jobject instance, jstring source_) {
     const char *source = env->GetStringUTFChars(source_, 0);
-    if (sPlayer != nullptr) {
-        sPlayer->setSource(new std::string(source));
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->setSource(new std::string(source));
+//    }
     env->ReleaseStringUTFChars(source_, source);
 }
 
@@ -52,26 +52,26 @@ extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeCreate(
 
     LOGD(JNI_TAG, "nativeInit");
 
-    if (sPlayer == nullptr) {
-
-        if (sJavaMethods != nullptr) {
-            delete sJavaMethods;
-            sJavaMethods = nullptr;
-        }
-
-        sJavaMethods = new SJavaMethods(sJavaVM, env, instance);
-
-        sPlayer = new SPlayer(sJavaVM, env, instance, sJavaMethods);
-
-        sStatus = sPlayer->getPlayerStatus();
-
-        if (sStatus != nullptr) {
-            sStatus->moveStatusToCreate();
-        }
-        if (sJavaMethods != nullptr) {
-            sJavaMethods->onCallJavaCreate();
-        }
-    }
+//    if (sPlayer == nullptr) {
+//
+//        if (sJavaMethods != nullptr) {
+//            delete sJavaMethods;
+//            sJavaMethods = nullptr;
+//        }
+//
+//        sJavaMethods = new SJavaMethods(sJavaVM, env, instance);
+//
+//        sPlayer = new SPlayer(sJavaVM, env, instance, sJavaMethods);
+//
+//        sStatus = sPlayer->getPlayerStatus();
+//
+//        if (sStatus != nullptr) {
+//            sStatus->moveStatusToCreate();
+//        }
+//        if (sJavaMethods != nullptr) {
+//            sJavaMethods->onCallJavaCreate();
+//        }
+//    }
 
     mediaPlayer = ((MediaPlayer *) new AndroidMediaPlayer());
     mediaPlayer->create();
@@ -82,100 +82,100 @@ extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeCreate(
 extern "C" JNIEXPORT void JNICALL
 Java_com_bzh_splayer_lib_SPlayer_nativeStart(JNIEnv *env, jobject instance) {
     LOGD(JNI_TAG, "nativeStart");
-    if (sPlayer != nullptr) {
-        sPlayer->start();
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->start();
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativePlay(JNIEnv *env, jobject instance) {
     LOGD(JNI_TAG, "nativePlay");
-    if (sPlayer != nullptr) {
-        sPlayer->play();
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->play();
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativePause(JNIEnv *env, jobject instance) {
     LOGD(JNI_TAG, "nativePause");
-    if (sPlayer != nullptr) {
-        sPlayer->pause();
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->pause();
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeStop(JNIEnv *env, jobject instance) {
     LOGD(JNI_TAG, "nativeStop");
-    if (sPlayer != nullptr) {
-        sPlayer->stop();
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->stop();
+//    }
 }
 
-void *destroyCallBack(void *data) {
-    if (data != nullptr) {
-        SStatus *status = (SStatus *) data;
-        while (!status->isStop()) {
-            continue;
-        }
-        status->moveStatusToPreDestroy();
-        if (status->isPreDestroy()) {
-            sStatus = nullptr;
-            delete sPlayer;
-            sPlayer = nullptr;
-        }
-    }
-    sIsExiting = false;
-    pthread_exit(&sDestroyThread);
-}
+//void *destroyCallBack(void *data) {
+//    if (data != nullptr) {
+//        SStatus *status = (SStatus *) data;
+//        while (!status->isStop()) {
+//            continue;
+//        }
+//        status->moveStatusToPreDestroy();
+//        if (status->isPreDestroy()) {
+//            sStatus = nullptr;
+//            delete sPlayer;
+//            sPlayer = nullptr;
+//        }
+//    }
+//    sIsExiting = false;
+//    pthread_exit(&sDestroyThread);
+//}
 
 extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeDestroy(JNIEnv *env, jobject instance) {
     LOGD(JNI_TAG, "nativeRelease");
-    if (sPlayer != nullptr && sStatus != nullptr && !sIsExiting) {
-        if (sStatus->isCreate() || sStatus->isPreCreate() || sStatus->isSource() || sStatus->isComplete()) {
-            sStatus = nullptr;
-
-            delete sPlayer;
-            sPlayer = nullptr;
-
-            delete sJavaMethods;
-            sJavaMethods = nullptr;
-        } else {
-            sPlayer->stop();
-            pthread_create(&sDestroyThread, nullptr, destroyCallBack, sStatus);
-            sIsExiting = true;
-        }
-    }
+//    if (sPlayer != nullptr && sStatus != nullptr && !sIsExiting) {
+//        if (sStatus->isCreate() || sStatus->isPreCreate() || sStatus->isSource() || sStatus->isComplete()) {
+//            sStatus = nullptr;
+//
+//            delete sPlayer;
+//            sPlayer = nullptr;
+//
+//            delete sJavaMethods;
+//            sJavaMethods = nullptr;
+//        } else {
+//            sPlayer->stop();
+//            pthread_create(&sDestroyThread, nullptr, destroyCallBack, sStatus);
+//            sIsExiting = true;
+//        }
+//    }
     delete mediaPlayer;
     mediaPlayer = nullptr;
 }
 
 extern "C" JNIEXPORT jint JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeGetTotalTimeMillis(JNIEnv *env,
                                                                                             jobject instance) {
-    if (sPlayer != nullptr) {
-        SFFmpeg *ffmpeg = sPlayer->getFFmpeg();
-        if (ffmpeg != nullptr) {
-            return (int) ffmpeg->getTotalTimeMillis();
-        }
-    }
+//    if (sPlayer != nullptr) {
+//        SFFmpeg *ffmpeg = sPlayer->getFFmpeg();
+//        if (ffmpeg != nullptr) {
+//            return (int) ffmpeg->getTotalTimeMillis();
+//        }
+//    }
     return 0;
 }
 
 extern "C" JNIEXPORT jint JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeGetCurrentTimeMillis(JNIEnv *env,
                                                                                               jobject instance) {
-    if (sPlayer != nullptr) {
-        SFFmpeg *ffmpeg = sPlayer->getFFmpeg();
-        if (ffmpeg != nullptr) {
-            return (int) ffmpeg->getCurrentTimeMillis();
-        }
-    }
+//    if (sPlayer != nullptr) {
+//        SFFmpeg *ffmpeg = sPlayer->getFFmpeg();
+//        if (ffmpeg != nullptr) {
+//            return (int) ffmpeg->getCurrentTimeMillis();
+//        }
+//    }
     return 0;
 }
 
 extern "C" JNIEXPORT jint JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeGetCurrentVolumePercent(JNIEnv *env,
                                                                                                  jobject instance) {
-    if (sPlayer != nullptr) {
-        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
-        if (openSLES != nullptr) {
-            return (int) openSLES->getCurrentVolumePercent();
-        }
-    }
+//    if (sPlayer != nullptr) {
+//        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
+//        if (openSLES != nullptr) {
+//            return (int) openSLES->getCurrentVolumePercent();
+//        }
+//    }
     return 0;
 }
 
@@ -183,18 +183,18 @@ extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeSeek(JN
                                                                               jobject instance,
                                                                               jint millis) {
     LOGD(JNI_TAG, "nativeSeek %d", (int) millis);
-    if (sPlayer != nullptr) {
-        sPlayer->seek((int64_t) millis);
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->seek((int64_t) millis);
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeVolume(JNIEnv *env,
                                                                                 jobject instance,
                                                                                 jint percent) {
     LOGD(JNI_TAG, "nativeVolume %d", (int) percent);
-    if (sPlayer != nullptr) {
-        sPlayer->volume((int) percent);
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->volume((int) percent);
+//    }
 }
 
 
@@ -202,48 +202,48 @@ extern "C" JNIEXPORT void JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeMute(JN
                                                                               jobject instance,
                                                                               jint mute) {
     LOGD(JNI_TAG, "nativeMute %d", (int) mute);
-    if (sPlayer != nullptr) {
-        sPlayer->mute((int) mute);
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->mute((int) mute);
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_bzh_splayer_lib_SPlayer_nativeSpeed(JNIEnv *env, jobject instance, jdouble speed) {
     LOGD(JNI_TAG, "nativeSpeed %f", (double) speed);
-    if (sPlayer != nullptr) {
-        sPlayer->speed((double) speed);
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->speed((double) speed);
+//    }
 }
 
 extern "C" JNIEXPORT void JNICALL
 Java_com_bzh_splayer_lib_SPlayer_nativePitch(JNIEnv *env, jobject instance, jdouble pitch) {
     LOGD(JNI_TAG, "nativePitch %f", (double) pitch);
-    if (sPlayer != nullptr) {
-        sPlayer->pitch((double) pitch);
-    }
+//    if (sPlayer != nullptr) {
+//        sPlayer->pitch((double) pitch);
+//    }
 }
 
 
 extern "C" JNIEXPORT jdouble JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeGetCurrentSpeed(JNIEnv *env,
                                                                                             jobject instance) {
-    if (sPlayer != nullptr) {
-        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
-        if (openSLES != nullptr) {
-            return (int) openSLES->getSoundSpeed();
-        }
-    }
+//    if (sPlayer != nullptr) {
+//        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
+//        if (openSLES != nullptr) {
+//            return (int) openSLES->getSoundSpeed();
+//        }
+//    }
     return 0;
 }
 
 
 extern "C" JNIEXPORT jdouble JNICALL Java_com_bzh_splayer_lib_SPlayer_nativeGetCurrentPitch(JNIEnv *env,
                                                                                             jobject instance) {
-    if (sPlayer != nullptr) {
-        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
-        if (openSLES != nullptr) {
-            return (int) openSLES->getSoundPitch();
-        }
-    }
+//    if (sPlayer != nullptr) {
+//        SOpenSLES *openSLES = sPlayer->getSOpenSLES();
+//        if (openSLES != nullptr) {
+//            return (int) openSLES->getSoundPitch();
+//        }
+//    }
     return 0;
 }
 #pragma clang diagnostic pop
--- splayer/src/main/cpp/splayer/media/Message.h
+++ splayer/src/main/cpp/splayer/media/Message.h
@@ -14,86 +14,80 @@ public:
     int arg2;
     void *obj;
 
+    /* 0~9999 */
+
     static const int MSG_FLUSH = 0;
-    static const int MSG_ERROR = 100; /* arg1 = error */
-    static const int MSG_PREPARED = 200;
-    static const int MSG_COMPLETED = 300;
-    static const int MSG_VIDEO_SIZE_CHANGED = 400; /* arg1 = width, arg2 = height */
-    static const int MSG_SAR_CHANGED = 401;        /* arg1 = sar.num, arg2 = sar.den */
-    static const int MSG_VIDEO_RENDERING_START = 402;
-    static const int MSG_AUDIO_RENDERING_START = 403;
-    static const int MSG_VIDEO_ROTATION_CHANGED = 404; /* arg1 = degree */
-    static const int MSG_AUDIO_DECODED_START = 405;
-    static const int MSG_VIDEO_DECODED_START = 406;
-    static const int MSG_OPEN_INPUT = 407;
-    static const int MSG_FIND_STREAM_INFO = 408;
-    static const int MSG_COMPONENT_OPEN = 409;
-    static const int MSG_VIDEO_SEEK_RENDERING_START = 410;
-    static const int MSG_AUDIO_SEEK_RENDERING_START = 411;
+    static const int MSG_ERROR = 1; /* arg1 = error */
+    static const int MSG_PREPARED = 2;
+    static const int MSG_COMPLETED = 3;
+    static const int MSG_VIDEO_SIZE_CHANGED = 4; /* arg1 = width, arg2 = height */
+    static const int MSG_SAR_CHANGED = 5;        /* arg1 = sar.num, arg2 = sar.den */
+    static const int MSG_VIDEO_RENDERING_START = 6;
+    static const int MSG_AUDIO_RENDERING_START = 7;
+    static const int MSG_VIDEO_ROTATION_CHANGED = 8; /* arg1 = degree */
+    static const int MSG_AUDIO_DECODED_START = 9;
+    static const int MSG_VIDEO_DECODED_START = 10;
+    static const int MSG_OPEN_INPUT = 11;
+    static const int MSG_FIND_STREAM_INFO = 12;
+    static const int MSG_COMPONENT_OPEN = 13;
+    static const int MSG_VIDEO_SEEK_RENDERING_START = 14;
+    static const int MSG_AUDIO_SEEK_RENDERING_START = 15;
+    static const int MSG_BUFFERING_START = 16;
+    static const int MSG_BUFFERING_END = 17;
+    static const int MSG_BUFFERING_UPDATE = 18;       /* arg1 = buffering head position in time, arg2 = minimum percent in time or bytes */
+    static const int MSG_BUFFERING_BYTES_UPDATE = 19; /* arg1 = cached data in bytes,            arg2 = high water mark */
+    static const int MSG_BUFFERING_TIME_UPDATE = 20;  /* arg1 = cached duration in milliseconds, arg2 = high water mark */
+    static const int MSG_SEEK_COMPLETE = 21;          /* arg1 = seek position,                   arg2 = error */
+    static const int MSG_PLAYBACK_STATE_CHANGED = 22;
+    static const int MSG_TIMED_TEXT = 23;
+    static const int MSG_ACCURATE_SEEK_COMPLETE = 24; /* arg1 = current position*/
+    static const int MSG_GET_IMG_STATE = 25;         /* arg1 = timestamp, arg2 = result code, obj = file name*/
+    static const int MSG_VIDEO_DECODER_OPEN = 26;
 
-    static const int MSG_BUFFERING_START = 500;
-    static const int MSG_BUFFERING_END = 501;
-    static const int MSG_BUFFERING_UPDATE = 502;       /* arg1 = buffering head position in time, arg2 = minimum percent in time or bytes */
-    static const int MSG_BUFFERING_BYTES_UPDATE = 503; /* arg1 = cached data in bytes,            arg2 = high water mark */
-    static const int MSG_BUFFERING_TIME_UPDATE = 504;  /* arg1 = cached duration in milliseconds, arg2 = high water mark */
-    static const int MSG_SEEK_COMPLETE = 600;          /* arg1 = seek position,                   arg2 = error */
-    static const int MSG_PLAYBACK_STATE_CHANGED = 700;
-    static const int MSG_TIMED_TEXT = 800;
-    static const int MSG_ACCURATE_SEEK_COMPLETE = 900; /* arg1 = current position*/
-    static const int MSG_GET_IMG_STATE = 1000;         /* arg1 = timestamp, arg2 = result code, obj = file name*/
-
-    static const int MSG_VIDEO_DECODER_OPEN = 10001;
+    /* REQ 20000 ~ 29999 */
 
     static const int REQ_START = 20001;
     static const int REQ_PAUSE = 20002;
     static const int REQ_SEEK = 20003;
 
-    static const int PROP_FLOAT_VIDEO_DECODE_FRAMES_PER_SECOND = 10001;
-    static const int PROP_FLOAT_VIDEO_OUTPUT_FRAMES_PER_SECOND = 10002;
-    static const int PROP_FLOAT_PLAYBACK_RATE = 10003;
-    static const int PROP_FLOAT_PLAYBACK_VOLUME = 10006;
-    static const int PROP_FLOAT_AVDELAY = 10004;
-    static const int PROP_FLOAT_AVDIFF = 10005;
-    static const int PROP_FLOAT_DROP_FRAME_RATE = 10007;
+    /* PROP 30000~39999 */
 
-    static const int PROP_INT64_SELECTED_VIDEO_STREAM = 20001;
-    static const int PROP_INT64_SELECTED_AUDIO_STREAM = 20002;
-    static const int PROP_INT64_SELECTED_TIMEDTEXT_STREAM = 20011;
-    static const int PROP_INT64_VIDEO_DECODER = 20003;
-    static const int PROP_INT64_AUDIO_DECODER = 20004;
-    static const int PROPV_DECODER_UNKNOWN = 0;
-    static const int PROPV_DECODER_AVCODEC = 1;
-    static const int PROPV_DECODER_MEDIACODEC = 2;
-    static const int PROPV_DECODER_VIDEOTOOLBOX = 3;
-    static const int PROP_INT64_VIDEO_CACHED_DURATION = 20005;
-    static const int PROP_INT64_AUDIO_CACHED_DURATION = 20006;
-    static const int PROP_INT64_VIDEO_CACHED_BYTES = 20007;
-    static const int PROP_INT64_AUDIO_CACHED_BYTES = 20008;
-    static const int PROP_INT64_VIDEO_CACHED_PACKETS = 20009;
-    static const int PROP_INT64_AUDIO_CACHED_PACKETS = 20010;
-
-    static const int PROP_INT64_BIT_RATE = 20100;
-
-    static const int PROP_INT64_TCP_SPEED = 20200;
-
-    static const int PROP_INT64_ASYNC_STATISTIC_BUF_BACKWARDS = 20201;
-    static const int PROP_INT64_ASYNC_STATISTIC_BUF_FORWARDS = 20202;
-    static const int PROP_INT64_ASYNC_STATISTIC_BUF_CAPACITY = 20203;
-    static const int PROP_INT64_TRAFFIC_STATISTIC_BYTE_COUNT = 20204;
-
-    static const int PROP_INT64_LATEST_SEEK_LOAD_DURATION = 20300;
-
-    static const int PROP_INT64_CACHE_STATISTIC_PHYSICAL_POS = 20205;
-
-    static const int PROP_INT64_CACHE_STATISTIC_FILE_FORWARDS = 20206;
-
-    static const int PROP_INT64_CACHE_STATISTIC_FILE_POS = 20207;
-
-    static const int PROP_INT64_CACHE_STATISTIC_COUNT_BYTES = 20208;
-
-    static const int PROP_INT64_LOGICAL_FILE_SIZE = 20209;
-    static const int PROP_INT64_SHARE_CACHE_DATA = 20210;
-    static const int PROP_INT64_IMMEDIATE_RECONNECT = 20211;
+    static const int PROP_FLOAT_VIDEO_DECODE_FRAMES_PER_SECOND = 30001;
+    static const int PROP_FLOAT_VIDEO_OUTPUT_FRAMES_PER_SECOND = 30002;
+    static const int PROP_FLOAT_PLAYBACK_RATE = 30003;
+    static const int PROP_FLOAT_AVDELAY = 30004;
+    static const int PROP_FLOAT_AVDIFF = 30005;
+    static const int PROP_FLOAT_PLAYBACK_VOLUME = 30006;
+    static const int PROP_FLOAT_DROP_FRAME_RATE = 30007;
+    static const int PROP_INT64_SELECTED_VIDEO_STREAM = 30008;
+    static const int PROP_INT64_SELECTED_AUDIO_STREAM = 30009;
+    static const int PROP_INT64_SELECTED_TIMEDTEXT_STREAM = 3010;
+    static const int PROP_INT64_VIDEO_DECODER = 30011;
+    static const int PROP_INT64_AUDIO_DECODER = 30012;
+    static const int PROP_V_DECODER_UNKNOWN = 30013;
+    static const int PROP_V_DECODER_AVCODEC = 30014;
+    static const int PROP_V_DECODER_MEDIACODEC = 30015;
+    static const int PROP_V_DECODER_VIDEOTOOLBOX = 30016;
+    static const int PROP_INT64_VIDEO_CACHED_DURATION = 30017;
+    static const int PROP_INT64_AUDIO_CACHED_DURATION = 30018;
+    static const int PROP_INT64_VIDEO_CACHED_BYTES = 30019;
+    static const int PROP_INT64_AUDIO_CACHED_BYTES = 30020;
+    static const int PROP_INT64_VIDEO_CACHED_PACKETS = 30021;
+    static const int PROP_INT64_AUDIO_CACHED_PACKETS = 30022;
+    static const int PROP_INT64_ASYNC_STATISTIC_BUF_BACKWARDS = 30023;
+    static const int PROP_INT64_ASYNC_STATISTIC_BUF_FORWARDS = 30024;
+    static const int PROP_INT64_ASYNC_STATISTIC_BUF_CAPACITY = 30025;
+    static const int PROP_INT64_TRAFFIC_STATISTIC_BYTE_COUNT = 30026;
+    static const int PROP_INT64_LATEST_SEEK_LOAD_DURATION = 30027;
+    static const int PROP_INT64_CACHE_STATISTIC_PHYSICAL_POS = 30028;
+    static const int PROP_INT64_CACHE_STATISTIC_FILE_FORWARDS = 30029;
+    static const int PROP_INT64_CACHE_STATISTIC_FILE_POS = 30030;
+    static const int PROP_INT64_CACHE_STATISTIC_COUNT_BYTES = 30031;
+    static const int PROP_INT64_LOGICAL_FILE_SIZE = 30032;
+    static const int PROP_INT64_SHARE_CACHE_DATA = 30033;
+    static const int PROP_INT64_IMMEDIATE_RECONNECT = 30034;
+    static const int PROP_INT64_BIT_RATE = 30035;
+    static const int PROP_INT64_TCP_SPEED = 30036;
 
     static const char *getMsgSimpleName(int what);
 };
