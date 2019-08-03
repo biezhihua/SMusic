0809d426b3853881e68157cac857228d23464012
biezhihua <biezhihua@gmail.com>
2019-08-03 09:38:19 +0800

    feat: update gitignore

--- splayer_mac/src/main.cpp
+++ splayer_mac/src/main.cpp
@@ -8,12 +8,12 @@
  */
 int main() {
 
-    if (SDL_Init(SDL_INIT_VIDEO) != 0){
+    if (SDL_Init(SDL_INIT_AUDIO | SDL_INIT_VIDEO) != 0) {
         std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
         return 1;
     }
     SDL_Quit();
-    
+
 //    auto *player = new MacMediaPlayer();
 //
 //    int ret = 0;
