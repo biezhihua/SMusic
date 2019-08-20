305dfce7699ee43ff6ab078d51ed068ffc27897c
biezhihua <biezhihua@gmail.com>
2019-08-20 21:23:49 +0800

    feat: core

---
+++ splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/src/MacAudio.cpp" "/Users/biezhihua/StudySpace/splayer/splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/src/MacAudio.cpp.o"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/src/MacMediaPlayer.cpp" "/Users/biezhihua/StudySpace/splayer/splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/src/MacMediaPlayer.cpp.o"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/src/MacOptions.cpp" "/Users/biezhihua/StudySpace/splayer/splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/src/MacOptions.cpp.o"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/src/MacStream.cpp" "/Users/biezhihua/StudySpace/splayer/splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/src/MacStream.cpp.o"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/src/MacSurface.cpp" "/Users/biezhihua/StudySpace/splayer/splayer_mac_demo/cmake-build-debug/splayer_mac/CMakeFiles/splayer_mac.dir/src/MacSurface.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "AppleClang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/include"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/../distribution/macos/ffmpeg/include"
+  "/Users/biezhihua/StudySpace/splayer/splayer_mac/../splayer_core/include"
+  "/usr/local/Cellar/sdl2/2.0.9_1/include/SDL2"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ splayer_mac_demo/cmake-build-debug/splayer_mac/splayer_core/CMakeFiles/splayer_core.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/splayer_core.dir/src/Audio.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Clock.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/CmdUtils.c.o"
+  "CMakeFiles/splayer_core.dir/src/Decoder.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/FFPlay.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/FrameQueue.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/MediaPlayer.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Message.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/MessageQueue.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Mutex.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Options.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/PacketQueue.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/PipelineNode.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/RefCount.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/State.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Stream.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Surface.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/Thread.cpp.o"
+  "CMakeFiles/splayer_core.dir/src/VOutOverlay.cpp.o"
+  "libsplayer_core.pdb"
+  "libsplayer_core.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C CXX)
+  include(CMakeFiles/splayer_core.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
