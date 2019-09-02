c670ccbc1fc3c697624c7bb5221f5f1a16b7fee1
biezhihua <biezhihua@gmail.com>
2019-09-02 23:40:09 +0800

    feat: demo complete

--- splayer_mac_demo/main.cpp
+++ splayer_mac_demo/main.cpp
@@ -1,10 +1,14 @@
 
+#include <MacMediaPlayer.h>
+
 int main() {
 //    MediaPlayer *mediaPlayer = MacMediaPlayer::create();
 //    mediaPlayer->create();
 //    mediaPlayer->setDataSource("/Users/biezhihua/Downloads/寄生虫.mp4");
 //    mediaPlayer->prepareAsync();
 //    return mediaPlayer->eventLoop();
+
+    MacMediaPlayer *mediaPlayer = new MacMediaPlayer();
 
     return 0;
 }
