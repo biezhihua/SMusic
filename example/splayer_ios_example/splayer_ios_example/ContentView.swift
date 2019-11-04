a517a419abe0afd9d75cabe263344e9e90763f29
biezhihua <biezhihua@gmail.com>
2019-11-04 22:41:13 +0800

    feat: ios demo

---
+++ example/splayer_ios_example/splayer_ios_example/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  splayer_ios_example
+//
+//  Created by biezhihua on 2019/11/4.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
