4250c84f9db54e042295e0c488d64210d2805c23
biezhihua <biezhihua@gmail.com>
2019-11-06 19:23:33 +0800

    feat: ios

---
+++ splayer_ios/splayer_ios/IOSMediaPlayer.swift
@@ -0,0 +1,20 @@
+//
+//  IOSMediaPlayer.swift
+//  splayer_ios
+//
+//  Created by biezhihua on 2019/11/6.
+//  Copyright © 2019 biezhihua. All rights reserved.
+//
+
+import Foundation
+
+public class IOSMediaPlayer {
+    
+    public init() {
+        
+    }
+    
+    public func play(){
+        print("play")
+    }
+}
---
+++ splayer_ios/splayer_ios/splayer_ios.h
@@ -0,0 +1,19 @@
+//
+//  splayer_ios.h
+//  splayer_ios
+//
+//  Created by biezhihua on 2019/11/6.
+//  Copyright © 2019 biezhihua. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for splayer_ios.
+FOUNDATION_EXPORT double splayer_iosVersionNumber;
+
+//! Project version string for splayer_ios.
+FOUNDATION_EXPORT const unsigned char splayer_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <splayer_ios/PublicHeader.h>
+
+
