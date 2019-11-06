869211ef3f57bdb5757ec9b81e33a89453b68651
biezhihua <biezhihua@gmail.com>
2019-11-06 22:58:51 +0800

    feat: create ios folder

--- splayer_ios/splayer_ios/birdge/module_birdge.h
+++ splayer_ios/splayer_ios/birdge/module_birdge.h
@@ -1,14 +1,14 @@
 //
-//  SwiftCpp.h
+//  birdge.h
 //  splayer_ios
 //
 //  Created by biezhihua on 2019/11/6.
 //  Copyright © 2019 biezhihua. All rights reserved.
 //
 
-#ifndef SwiftCpp_h
-#define SwiftCpp_h
+#ifndef module_birdge_h
+#define module_birdge_h
 
 #import "splayer_ios_birdge_header.h"
 
-#endif /* SwiftCpp_h */
+#endif /* module_birdge_h */
