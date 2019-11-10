d0e10092304dc83e6fa29f43dd8bb86a08ee6144
biezhihua <biezhihua@gmail.com>
2019-11-10 15:16:18 +0800

    fix: ffmpeg static library

--- splayer_android/src/main/java/com/bzh/splayer/MeidaPlayer.kt
+++ splayer_android/src/main/java/com/bzh/splayer/MeidaPlayer.kt
@@ -509,13 +509,13 @@ class MediaPlayer : IMediaPlayer {
         val OPT_CATEGORY_SWR = 5       // 音频重采样参数
 
         init {
-            System.loadLibrary("libavcodec")
-            System.loadLibrary("libavdevice")
-            System.loadLibrary("libavfilter")
-            System.loadLibrary("libavformat")
-            System.loadLibrary("libavutil")
-            System.loadLibrary("libswresample")
-            System.loadLibrary("libswscale")
+            System.loadLibrary("avcodec")
+            System.loadLibrary("avdevice")
+            System.loadLibrary("avfilter")
+            System.loadLibrary("avformat")
+            System.loadLibrary("avutil")
+            System.loadLibrary("swresample")
+            System.loadLibrary("swscale")
             System.loadLibrary("splayer_soundtouch")
             System.loadLibrary("splayer_renderer")
             System.loadLibrary("splayer_engine")
