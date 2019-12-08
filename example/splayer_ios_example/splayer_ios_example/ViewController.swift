63b84ca5af8b821fc42d6e8e2ae05d647cfe1a82
biezhihua <biezhihua@gmail.com>
2019-12-08 18:08:51 +0800

    feat: bind action

--- example/splayer_ios_example/splayer_ios_example/ViewController.swift
+++ example/splayer_ios_example/splayer_ios_example/ViewController.swift
@@ -11,16 +11,46 @@ import splayer_ios
 
 class ViewController: UIViewController {
 
-    
-    var create:UIButton?
-    
+    @IBOutlet weak var create: UIButton!
+
+    @IBOutlet weak var start: UIButton!
+
+    @IBOutlet weak var play: UIButton!
+
+    @IBOutlet weak var pause: UIButton!
+
+    @IBOutlet weak var stop: UIButton!
+
+    @IBOutlet weak var destroy: UIButton!
+
+    @IBAction func onCreate() {
+        Log.d("Main", "onCreate")
+    }
+
+    @IBAction func onStart() {
+        Log.d("Main", "onStart")
+    }
+
+    @IBAction func onPlay() {
+        Log.d("Main", "onPlay")
+    }
+
+    @IBAction func onPuase() {
+        Log.d("Main", "onPuase")
+    }
+
+    @IBAction func onStop() {
+        Log.d("Main", "onStop")
+    }
+
+    @IBAction func onDestroy() {
+        Log.d("Main", "onDestroy")
+    }
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        let mp = MediaPlayer()
-    
     }
-
 
 }
 
